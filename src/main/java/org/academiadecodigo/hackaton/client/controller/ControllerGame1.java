54a0459ba0d3c29b3be32de4ff5b559ff547c3d7
Pauwinho <p.sousa.design@gmail.com>
2017-07-13 19:28:19 +0100

    create menu view and game 1

---
+++ src/main/java/org/academiadecodigo/hackaton/client/controller/ControllerGame1.java
@@ -0,0 +1,17 @@
+package org.academiadecodigo.hackaton.client.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by codecadet on 13/07/17.
+ */
+public class ControllerGame1 extends Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("open");
+    }
+}
