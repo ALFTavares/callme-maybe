bd762d0273a11817b9cde2eb6e0ce274ab1a2501
mouse <alftavares1985@gmail.com>
2017-07-13 17:28:49 +0100

    added score pojo to server

---
+++ src/main/java/server/Score.java
@@ -0,0 +1,38 @@
+package server;
+
+/**
+ * Created by bob on 13-07-2017.
+ */
+public class Score {
+    private String name;
+    private int score;
+    private long id;
+
+    public Score() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
