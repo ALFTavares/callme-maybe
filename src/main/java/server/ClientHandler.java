4b392eba7512eb943567453ad28beefe7518d67d
mouse <alftavares@gmail.com>
2017-07-13 18:12:13 +0100

    added socket to clientHandler

--- src/main/java/server/ClientHandler.java
+++ src/main/java/server/ClientHandler.java
@@ -1,16 +1,27 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 /**
  * Created by bob on 13-07-2017.
  */
 public class ClientHandler implements Runnable {
     private Server server;
+    private Socket socket;
 
-    public ClientHandler(Server server) {
+    public ClientHandler(Server server, Socket socket) {
         this.server = server;
+        this.socket = socket;
     }
 
     public void run() {
-
+        try {
+            BufferedReader bReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
